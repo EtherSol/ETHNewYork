460a534189fd4f5e3142a062f7372ba9a2f94f78
Kyle Matthew Zhou <kylemz@email.arizona.edu>
2022-06-25 21:53:53 -0400

    Django

--- Website/Web3/contracts/MetroToken.sol
+++ Website/Web3/contracts/MetroToken.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol"
 
-contract MetroToken is ERC20 {
+contract MetroToken is ERC20Votes {
     constructor(uint256 initialSupply) ERC20("MetroToken", "MTNY") {
         _mint(msg.sender, initialSupply);
     }
--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"Website/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
